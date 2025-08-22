009c12d8d00986597c43451479564fe8f69f2204
sophiabarros <soohbarros1@gmail.com>
2025-08-21 22:45:32 -0300

    adição do html da linha do tempo, finalização do html learn - furacão e início do css da learn furacão

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-new Siema()
+const slider = new Siema()
+
+const buttonPrev = document.querySelector('#prev')
+const buttonNext = document.querySelector('#next')
+
+buttonPrev.addEventListener('click', () => slider.prev())
+buttonNext.addEventListener('click', () => slider.next())
+
+//cria outro js p mexer
