abaae9744335c62ffcd81e949ac1bf6da58edc7e
sophiabarros <soohbarros1@gmail.com>
2025-08-23 19:33:25 -0300

    finalização da página de learn do furacão.

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const slider = new Siema()
 
 const buttonPrev = document.querySelector('#prev')
 const buttonNext = document.querySelector('#next')
+const btnUp = document.getElementById("up")
 
 buttonPrev.addEventListener('click', () => slider.prev())
 buttonNext.addEventListener('click', () => slider.next())
+
+btnUp.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  })
 
 //cria outro js p mexer
