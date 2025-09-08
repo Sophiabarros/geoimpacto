e197ac0552b80120a4a4144659974abf64795fe5
sophiabarros <soohbarros1@gmail.com>
2025-09-08 20:37:59 -0300

    carrossel de escolha de conteúdo pronto

--- main.js
+++ main.js
@@ -11,4 +11,33 @@ btnUp.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" })
   })
 
-  //cria outro js p mexer
+  //cria outro js p mexerconst carousel = document.querySelector('.selecao');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  carousel.classList.add('grabbing');
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+  carousel.classList.remove('grabbing');
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+  carousel.classList.remove('grabbing');
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return; // só arrasta se o mouse estiver pressionado
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 1.2; // multiplicador = sensibilidade
+  carousel.scrollLeft = scrollLeft - walk;
+});
